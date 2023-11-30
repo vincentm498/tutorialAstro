d33a2f98d298d85fdf6651a20843a63c9f078912
Vincent <50195411+vincentm498@users.noreply.github.com>
2023-11-30 16:24:25 +0100

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [expressiveCode(), preact()]
+  integrations: [expressiveCode(), preact()],
+  scopedStyleStrategy: 'class',
+  build: {
+    inlineStylesheets: `never`,
+  },
 });
